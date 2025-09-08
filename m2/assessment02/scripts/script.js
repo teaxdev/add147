d2494ec54f6f92dc6fc1ea6867b77de967560dac
teaxdev <FloresAlex6904@gmail.com>
2025-09-08 16:10:14 -0500

    finish assessment02

--- m2/assessment02/scripts/script.js
+++ m2/assessment02/scripts/script.js
@@ -1,7 +1,7 @@
 const magic1 = {
     name: "Prosper, Tome-Bound",
     color: "Rakdos",
-    cost: "4",
+    cost: "Mana Cost: 4",
     type: "Legendary Creature",
     use: "It is a great card to use as an engine (A card that you use as to gain value, like drawing cards or creating treasure)",
     img: "images/prosper.jpg"
@@ -11,7 +11,7 @@ const magic1 = {
 const magic2 = {
     name: "Wilhelt, The Rotcleaver",
     color: "Dimir",
-    cost: "4",
+    cost: "Mana Cost: 4",
     type: "Legendary Creature",
     use: "He is like an insurance company, that gives you a zombie back when you lose one, and lets you draw cards every turn",
     img: "images/willy.jpg"
@@ -21,27 +21,27 @@ const magic2 = {
 const magic3 = {
     name: "Yuriko, The Tigers Shadow",
     color: "Dimir",
-    cost: "3",
+    cost: "Mana Cost: 3",
     type: "Legendary Creature",
     use: "Yuriko does damage to all your opponents depending on the mana cost of the top card of your deck",
-    img: "images/yuriko"
+    img: "images/yuriko.jpg"
 
 }
 
 const magic4 = {
     name: "Satoru Umezawa",
     color: "Dimir",
-    cost: "3",
+    cost: "Mana Cost: 3",
     type: "Legendary Creature",
     use: "Satoru makes all of your other creatures have ninjutsu, so that you cansneak them in to attack your opponents",
-    img: "images/satoru2"
+    img: "images/satoru2.jpg"
 
 }
 
 const magic5 = {
     name: "Gray Merchant of Asphodel",
     color: "Black",
-    cost: "4",
+    cost: "Mana Cost: 5",
     type: "Creature",
     use: "'Gary' as he is often called, deals damage to your opponents based on how many black pips you have on your cards in play",
     img: "images/gary.jpg"
@@ -52,17 +52,17 @@ function display() {
     let html1 = `<h2>${magic1.name}</h2>` + `<h3>${magic1.color}</h3>` + `<img src=${magic1.img}>` +
     `<h3></h3>` + `<p>${magic1.cost}</p>` + `<p>${magic1.type}</p>` + `<p>${magic1.use}</p>`;
 
-    let html2 = `<h2>${magic1.name}</h2>` + `<h3>${magic1.color}</h3>` + `<img src=${magic1.img}>` +
-    `<h3></h3>` + `<p>${magic1.cost}</p>` + `<p>${magic1.type}</p>` + `<p>${magic1.use}</p>`;
+    let html2 = `<h2>${magic2.name}</h2>` + `<h3>${magic2.color}</h3>` + `<img src=${magic2.img}>` +
+    `<h3></h3>` + `<p>${magic2.cost}</p>` + `<p>${magic2.type}</p>` + `<p>${magic2.use}</p>`;
 
-    let html3 = `<h2>${magic1.name}</h2>` + `<h3>${magic1.color}</h3>` + `<img src=${magic1.img}>` +
-    `<h3></h3>` + `<p>${magic1.cost}</p>` + `<p>${magic1.type}</p>` + `<p>${magic1.use}</p>`;
+    let html3 = `<h2>${magic3.name}</h2>` + `<h3>${magic3.color}</h3>` + `<img src=${magic3.img}>` +
+    `<h3></h3>` + `<p>${magic3.cost}</p>` + `<p>${magic3.type}</p>` + `<p>${magic3.use}</p>`;
 
-    let html4 = `<h2>${magic1.name}</h2>` + `<h3>${magic1.color}</h3>` + `<img src=${magic1.img}>` +
-    `<h3></h3>` + `<p>${magic1.cost}</p>` + `<p>${magic1.type}</p>` + `<p>${magic1.use}</p>`;
+    let html4 = `<h2>${magic4.name}</h2>` + `<h3>${magic4.color}</h3>` + `<img src=${magic4.img}>` +
+    `<h3></h3>` + `<p>${magic4.cost}</p>` + `<p>${magic4.type}</p>` + `<p>${magic4.use}</p>`;
 
-    let html5 = `<h2>${magic1.name}</h2>` + `<h3>${magic1.color}</h3>` + `<img src=${magic1.img}>` +
-    `<h3></h3>` + `<p>${magic1.cost}</p>` + `<p>${magic1.type}</p>` + `<p>${magic1.use}</p>`;
+    let html5 = `<h2>${magic5.name}</h2>` + `<h3>${magic5.color}</h3>` + `<img src=${magic5.img}>` +
+    `<h3></h3>` + `<p>${magic5.cost}</p>` + `<p>${magic5.type}</p>` + `<p>${magic5.use}</p>`;
 
     document.getElementById('ob1').innerHTML = html1;
     document.getElementById('ob2').innerHTML = html2;
