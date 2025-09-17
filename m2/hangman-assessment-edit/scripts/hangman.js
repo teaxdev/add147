f121106e8ee540df79605905f135de3d444f842d
teaxdev <FloresAlex6904@gmail.com>
2025-09-17 11:18:57 -0500

    finish hangman

--- m2/hangman-assessment-edit/scripts/hangman.js
+++ m2/hangman-assessment-edit/scripts/hangman.js
@@ -32,25 +32,45 @@ document.getElementById("submit").addEventListener("click", function(event){
     event.preventDefault();
     output = '';
     userLetter = document.getElementById("guess").value;
-    usedLetters.push(userLetter)
-    document.getElementById("guessed").value = usedLetters;
     document.getElementById("guess").value = ''; //would this reset value to null
-
+    if (usedLetters.includes(userLetter)) {
+        document.getElementById("used").innerHTML = "letter has already been used!";
+        document.getElementById("guessed").innerHTML = usedLetters;
+        return;
+    }
+    usedLetters.push(userLetter)
+    document.getElementById("guessed").innerHTML = usedLetters;
+    let found = false;
     for (let c = 0; c < answer.length; c++) {
         //alert(letters[c]);
-        found = false;
         if (userLetter.toUpperCase() == letters[c]) {
             display[c] = userLetter.toUpperCase();
             win--;
             found = true
-        } else if (found == false) { // send men
-            attemptsLeft--;
         }
-        output = output + display[c] + ' ';
+        output = output + display[c] + ' ';    
     }
-    
+    if (found == false) { // send men
+        attemptsLeft--;
+    }
     document.getElementById("word").innerHTML = output;
     output = '';
+
+    let img = document.getElementById("hangman"); 
+    if (attemptsLeft == 5) {
+        img.src="images/02.png";
+    } else if (attemptsLeft == 4) {
+        img.src="images/03.png";
+    } else if (attemptsLeft == 3) {
+        img.src="images/04.png";
+    } else if (attemptsLeft == 2) {
+        img.src="images/05.png";
+    } else if (attemptsLeft == 1) {
+        img.src="images/06.png";
+    } else {
+        img.src="images/07.png";
+    }
+
 
     if (win < 1) {
         document.getElementById("guesses").innerHTML = 'YOU WIN!!!';
@@ -61,5 +81,3 @@ document.getElementById("submit").addEventListener("click", function(event){
     }
 });
 
-
-//scirpt connected fine somereason the button and the script are not registering.
