627a45c34ab26aaa46c2ca4ee07c340efe00d166
teaxdev <FloresAlex6904@gmail.com>
2025-09-23 22:08:55 -0500

    finish hot cold

--- m2/assessment04/scripts/script.js
+++ m2/assessment04/scripts/script.js
@@ -1,11 +1,56 @@
+let value;
+
 function start() {
-    const value = Math.floor(Math.random() * 1000) + 1
-    alert(value)
-    let attempt = document.getElementById("guess").value;
+    value = Math.floor(Math.random() * 1000) + 1;
+    alert(value);
+}
+
+function hotcold() {
+    let attempt = parseInt(document.getElementById("guess").value);
     if (value == attempt) {
-        document.getElementById("random2").innerHTML = "You guessed correctly!";
-    } else if (attempt < value - 300) {
-        document.getElementById("random2").innerHTML = "You're freezing cold!";
+        document.getElementById("display").innerHTML = "You guessed correctly!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(67, 243, 51, 1)";
+    } else if (attempt < value - 400) {
+        document.getElementById("display").innerHTML = "You're freezing cold!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(74, 171, 255, 1)";
+    } else if (attempt < value - 200) {
+        document.getElementById("display").innerHTML = "You're cold!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(147, 205, 255, 1)";
+    } else if (attempt < value - 100) {
+        document.getElementById("display").innerHTML = "You're getting warm!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 210, 143, 1)";
+    } else if (attempt < value - 50) {
+        document.getElementById("display").innerHTML = "You're getting really warm!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 166, 34, 1)";
+    } else if (attempt < value - 20) {
+        document.getElementById("display").innerHTML = "You're getting hot!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 114, 58, 1)";
+    } else if (attempt < value - 10) {
+        document.getElementById("display").innerHTML = "You're piping hot!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 76, 52, 1)";
+    } else if (attempt < value - 5) {
+        document.getElementById("display").innerHTML = "You're BURNING!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 34, 34, 1)";
+    } else if (attempt > value + 400) {
+        document.getElementById("display").innerHTML = "You're freezing cold!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(74, 171, 255, 1)";
+    } else if (attempt > value + 200) {
+        document.getElementById("display").innerHTML = "You're cold!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(147, 205, 255, 1)";
+    } else if (attempt > value + 100) {
+        document.getElementById("display").innerHTML = "You're getting warm!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 210, 143, 1)";
+    } else if (attempt > value + 50) {
+        document.getElementById("display").innerHTML = "You're getting really warm!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 166, 34, 1)";
+    } else if (attempt > value + 20) {
+        document.getElementById("display").innerHTML = "You're getting hot!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 114, 58, 1)";
+    } else if (attempt > value + 10) {
+        document.getElementById("display").innerHTML = "You're piping hot!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 76, 52, 1)";
+    } else if (attempt > value + 5) {
+        document.getElementById("display").innerHTML = "You're BURNING!";
+        document.getElementById("displaytext").style.backgroundColor = "rgba(255, 34, 34, 1)";
     }
-    
 }
