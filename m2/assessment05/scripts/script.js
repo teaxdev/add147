47e9a3c3afc19bd4c72480ba1aa890996fb8bac7
Alex <FloresAlex6904@gmail.com>
2025-09-30 20:24:08 -0500

    finish assessment05

--- m2/assessment05/scripts/script.js
+++ m2/assessment05/scripts/script.js
@@ -5,8 +5,51 @@ months = [
 
 
 i = 1;
-while (i < months.length) {
-    document.getElementById(`month${i}`).innerHTML += m;
-    i++;
+for (m of months) {
+  var holidays = '';
+switch (m) {
+  case "January":
+    holidays = ["New Year's Day", "Martin Luther King Jr. Day"];
+    break;
+  case "February":
+    holidays = ["Presidents' Day", "Valentine's Day"];
+    break;
+  case "March":
+    holidays = ["St. Patrick's Day", "International Women's Day"];
+    break;
+  case "April":
+    holidays = ["Easter", "Earth Day"];
+    break;
+  case "May":
+    holidays = ["Memorial Day", "Mother's Day"];
+    break;
+  case "June":
+    holidays = ["Juneteenth", "Father's Day"];
+    break;
+  case "July":
+    holidays = ["Independence Day", "Parents' Day"];
+    break;
+  case "August":
+    holidays = ["Women's Equality Day", "International Youth Day"];
+    break;
+  case "September":
+    holidays = ["Labor Day", "Patriot Day"];
+    break;
+  case "October":
+    holidays = ["Columbus Day", "Halloween"];
+    break;
+  case "November":
+    holidays = ["Veterans Day", "Thanksgiving"];
+    break;
+  case "December":
+    holidays = ["Christmas", "Hanukkah"];
+    break;
+  default:
+    holidays = ["No holidays found"];
+    break;
 }
-
+  
+  document.getElementById(`month${i}`).innerHTML += `${m}<br><br>Holidays: ${holidays.join(", ")}`;
+  // console.log("loop working")
+  i++;
+}
